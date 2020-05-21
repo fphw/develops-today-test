a206b17c806bba7bfbb4c5df7ce6f58b59433654
andreymoyseev <andreymoyseev@gmail.com>
2020-05-21 09:43:38 +0300

    Change settings.py and requir...

--- newsBoard/newsBoard/settings.py
+++ newsBoard/newsBoard/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import dj_database_url
 import django_heroku
+import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +130,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 django_heroku.settings(locals())
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
